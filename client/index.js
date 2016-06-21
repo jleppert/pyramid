89dcf7a60f809746003ff786b38c273ed754400c
Johnathan Leppert <johnathan@kespry.com>
2016-06-21 00:53:26 -0700

    resize + datgui

--- client/index.js
+++ client/index.js
@@ -4,9 +4,9 @@ window.Events = require('backbone-events-standalone');
 window._ = require('underscore');
 var Webvs = require('./vendor/webvs.js');
 
+var dat = require('dat-gui');
+
 var visualizer = document.getElementById('visualizer');
-visualizer.width = window.innerWidth;
-visualizer.height = window.innerHeight;
 var analyser = new Webvs.WebAudioAnalyser();
 var webvs = new Webvs.Main({
   canvas: visualizer,
@@ -14,7 +14,44 @@ var webvs = new Webvs.Main({
   showStat: false
 });
 
-webvs.loadPreset(require('./presets/superscope'));
+function resize() {
+  visualizer.width = window.innerWidth;
+  visualizer.height = window.innerHeight;
+  webvs.notifyResize();
+}
+window.onresize = resize;
+resize();
+
+var State = function() {
+  this.station = 'disco_house';
+  this.vis = 'jello_cube';
+};
+
+var presets = {
+  anja: require('./presets/jello-cube'),
+  jello_cube: require('./presets/jello-cube'),
+  maze: require('./presets/maze'),
+  orgasms: require('./presets/orgasms'),
+  scope: require('./presets/scope'),
+  silkstring: require('./presets/silkstring'),
+  superscope: require('./presets/superscope')
+};
+
+var stations = {
+  disco_house: 'http://localhost:9001/http://prem1.di.fm:80/deepnudisco_hi?198f0d27e27e521ed98c303f',
+  vocal_trance: 'blah',
+  deep_nu_disco: 'blah2'
+};
+
+window.onload = function() {
+  var gui = new dat.GUI();
+  var app = new State();
+
+  gui.add(app, 'station', Object.keys(stations));
+  gui.add(app, 'vis', Object.keys(presets));
+}
+
+webvs.loadPreset(require('./presets/jello-cube'));
 
 webvs.start();
 analyser.load('http://localhost:9001/http://prem1.di.fm:80/deepnudisco_hi?198f0d27e27e521ed98c303f');
