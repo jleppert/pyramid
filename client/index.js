cdde4adbe71a20a051052a9bcd1851ca43313992
Johnathan Leppert <johnathan@kespry.com>
2016-11-23 11:45:53 -0800

    fix demos

--- client/index.js
+++ client/index.js
@@ -23,35 +23,56 @@ window.onresize = resize;
 resize();
 
 var State = function() {
-  this.station = 'disco_house';
+  this.station = 'dj_mixes';
   this.vis = 'jello_cube';
 };
 
 var presets = {
-  anja: require('./presets/jello-cube'),
   jello_cube: require('./presets/jello-cube'),
   maze: require('./presets/maze'),
-  orgasms: require('./presets/orgasms'),
   scope: require('./presets/scope'),
   silkstring: require('./presets/silkstring'),
   superscope: require('./presets/superscope')
 };
 
 var stations = {
-  disco_house: 'http://localhost:9001/http://prem1.di.fm:80/deepnudisco_hi?198f0d27e27e521ed98c303f',
-  vocal_trance: 'blah',
-  deep_nu_disco: 'blah2'
+  dj_mixes: 'http://prem1.di.fm:80/djmixes?198f0d27e27e521ed98c303f',
+  techno: 'http://prem1.di.fm:80/techno?198f0d27e27e521ed98c303f',
+  mainstage: 'http://prem4.di.fm:80/mainstage?198f0d27e27e521ed98c303f',
+  tropical_house: 'http://prem1.di.fm:80/chillntropicalhouse?198f0d27e27e521ed98c303f',
+  disco_house: 'http://prem1.di.fm:80/discohouse?198f0d27e27e521ed98c303f',
+  deep_nu_disco: 'http://prem1.di.fm:80/deepnudisco?198f0d27e27e521ed98c303f',
+  vocal_chillout: 'http://prem1.di.fm:80/vocalchillout?198f0d27e27e521ed98c303f',
+  vocal_trance: 'http://prem1.di.fm:80/vocaltrance?198f0d27e27e521ed98c303f',
+  vocal_lounge: 'http://prem1.di.fm:80/vocallounge?198f0d27e27e521ed98c303f'
 };
+
+var proxyPort = 9001;
+function audioSrc(key) {
+  return window.location.protocol + '//' + window.location.hostname + ':' + proxyPort + '/' + stations[key];
+}
 
 window.onload = function() {
   var gui = new dat.GUI();
   var app = new State();
+  
+  var audio = new Audio();
+  audio.setAttribute('crossorigin', 'anonymous');
+  audio.setAttribute('preload', 'auto');
+  audio.setAttribute('autoplay', 'autoplay');
+  audio.src = audioSrc(app.station);
 
-  gui.add(app, 'station', Object.keys(stations));
-  gui.add(app, 'vis', Object.keys(presets));
+  gui.add(app, 'station', Object.keys(stations)).onFinishChange(function(key) {
+    audio.src = audioSrc(key);
+  });
+  gui.add(app, 'vis', Object.keys(presets)).onFinishChange(function(key) {
+    webvs.loadPreset(presets[key]);  
+  });
+
+  webvs.loadPreset(presets[app.vis]);
+  analyser.load(audio);
+  
+  window.analyser = analyser;
+  
+  webvs.start();
 }
-
-webvs.loadPreset(require('./presets/jello-cube'));
-
-webvs.start();
-analyser.load('http://localhost:9001/http://prem1.di.fm:80/deepnudisco_hi?198f0d27e27e521ed98c303f');
--- index.js
+++ index.js
@@ -6,17 +6,17 @@ var corsProxy = require('cors-anywhere');
 var app = express();
 
 app.use('/', express.static(__dirname + '/static'));
-app.use('/client', browserify(__dirname + '/client'));
+app.use('/client', browserify(__dirname + '/client', { minify: true }));
 app.use('/swf', express.static(__dirname + '/node_modules/soundmanager2/swf'));
 
-app.listen(9000, function () {
+app.listen(1337, function () {
   console.log('Started listening on port 9000!');
 });
 
-corsProxy.createServer().listen(9001, '127.0.0.1', function() {
+corsProxy.createServer().listen(9001, function() {
   console.log('CORS proxy running');
 });
 
-var liveReloadServer = livereload.createServer();
-liveReloadServer.watch(__dirname + '/client');
-liveReloadServer.watch(__dirname + '/static');
+//var liveReloadServer = livereload.createServer();
+//liveReloadServer.watch(__dirname + '/client');
+//liveReloadServer.watch(__dirname + '/static');
